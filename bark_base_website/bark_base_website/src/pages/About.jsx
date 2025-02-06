807e8ee5246ee8a2e1aaa1573335bc6f3fa5d044
AL-JW <alex.woynich@gmail.com>
2025-02-06 15:09:26 -0600

    deleted this website

---
+++ bark_base_website/bark_base_website/src/pages/About.jsx
@@ -0,0 +1,13 @@
+function About() {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>
+        We are dedicated to training dogs using positive reinforcement
+        techniques.
+      </p>
+    </div>
+  );
+}
+
+export default About; // ✅ This line is required
---
+++ bark_base_website/bark_base_website/src/pages/Services.jsx
@@ -0,0 +1,14 @@
+function Services() {
+  return (
+    <div>
+      <h1>Our Training Programs</h1>
+      <ul>
+        <li>Puppy Training</li>
+        <li>Obedience Training</li>
+        <li>Behavior Correction</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Services; // ✅ Required
